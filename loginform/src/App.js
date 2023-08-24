c5eb207ca1c59675044261c816d3bb45883e2fa3
komal91 <komal.mathur91@gmail.com>
2023-08-24 13:10:13 +0300

    changes related to authentication and material-ui
    Changes in Login.js, App.js and new file added App.css for the changes related to  authentication and material-ui

--- loginform/src/App.js
+++ loginform/src/App.js
@@ -1,16 +1,53 @@
+import "./App.css";
 
+// Material UI imports
+import Chip from "@mui/material/Chip";
+import FaceIcon from "@mui/icons-material/Face";
+import Paper from "@mui/material/Paper";
+import LockIcon from "@mui/icons-material/Lock";
 
+import Switch from "@mui/material/Switch";
+import { useState } from "react";
 import Login from "./components/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./components/Login";
 
 function App() {
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <div className="App">
-    <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Login />} />
-            </Routes>
-          </BrowserRouter>
+      <Paper elevation={3} style={{ padding: "10px", paddingBottom: "50px" }}>
+        <div align="center">
+          {checked ? (
+            <Chip
+              icon={<LockIcon />}
+              label="Log In"
+              variant="outlined"
+              color="info"
+            />
+          ) : (
+            <Chip
+              icon={<FaceIcon />}
+              label="Sign Up"
+              variant="outlined"
+              color="info"
+            />
+          )}
+          <br />
+
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+        </div>
+
+        {checked ? <Login /> : <Signup />}
+      </Paper>
     </div>
   );
 }
